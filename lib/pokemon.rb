976aeccc4a2e774486ede9e0bf30cb6519643291
Hunter Sides <hunter.s.1337@gmail.com>
2020-08-18 23:11:21 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,10 +1,10 @@
 class Pokemon
-
   
-  attr_reader :id, :name, :type,:db
+  
+  attr_reader :id, :name, :type, :hp, :db
   @@all = []
 
-  def initialize (id:, name:, type:,db:)
+  def initialize (id:, name:, type:, hp: nil, db:)
     @id = id
     @name = name
     @type = type
