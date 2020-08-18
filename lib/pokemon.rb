6cc25ba6b453ff1eceb5edb1464c595ca1a235d6
Hunter Sides <hunter.s.1337@gmail.com>
2020-08-18 23:02:51 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -6,7 +6,6 @@ class Pokemon
     @id = id
     @name = name
     @type = type
-    @hp = hp
     @db = db
     @@all << self
   end
@@ -15,14 +14,14 @@ class Pokemon
     database_connection.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
   end
 
- # def self.find(id, database_connection)
-  #  pokemon = database_connection.execute("SELECT * FROM pokemon WHERE id = ?", id).flatten
-   # name = pokemon[1]
-  #  type = pokemon[2]
-  #  hp = pokemon[3]
+  def self.find(id, database_connection)
+    pokemon = database_connection.execute("SELECT * FROM pokemon WHERE id = ?", id).flatten
+    name = pokemon[1]
+    type = pokemon[2]
+   
 
-  #  pokemon_inst = Pokemon.new(id: id, name: name, type: type, hp: hp, db: database_connection)
- # end
+    pokemon_inst = Pokemon.new(id: id, name: name, type: type, db: database_connection)
+  end
 
   
 
